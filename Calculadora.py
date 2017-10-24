820bc17c55b43d0b19ba159f5b495a746737ae73
Marco Azúa Ulloa <marcoazuabiobio@gmail.com>
2017-10-24 12:35:49 -0400

    Calculadora
    Calculadora Basica hecha en python

--- Calculadora.py
+++ Calculadora.py
@@ -1,36 +1,50 @@
-
 print("*********  Calculadora beta 0.1 ***********")
-print("*           Por marco Azúa Ulloa          *")
-print("*********  Bienvenido Señor/a   ***********")
-
+print("*          Por marco Azúa Ulloa          *")
+print("*********   Bienvenido Señor/a   ***********")
 
 def sumar(a, b):
     return a + b
 
-
 def restar(a, b):
     return a - b
-
 
 def multiplicar(a, b):
     return a * b
 
-
 def dividir(a, b):
     return a / b
 
+def Menu():
+    print("Opciones: \n1.-Sumar\n2.-Restar\n3.-Multiplicar\n4.-Dividir\n5.-Salir")
 
-numero = int(float(input("Ingresa un numero: ")))
-numero_2 = int(float(input("Ingresa el segundo numero: ")))
-
-print("Opciones\n1.- Sumar\n2.-Restar\n3.-Multiplicar\n4.-Dividir")
-
-operaciones = {'1': sumar, '2': restar, '3': multiplicar, '4': dividir}
-
-seleccion = input('Ahora escoga una opcion: ')
+def MenuOpcion():
+    print("¿Desea hacer otra operacion?: \n1.-Si\n2.-No")
+    
+def no():
+    print("Gracias por usar la aplicacion")
 
 try:
-    resultado = operaciones[seleccion](int(numero), int(numero_2))
-    print(resultado)
+    while True:
+        numero = int(input("Ingresa un numero: "))
+        numero_2 = int(input("Ingresa el segundo numero: "))
+        Menu()
+        operaciones = {'1': sumar, '2': restar,
+                       '3': multiplicar, '4': dividir, '5': 'salir'}
+        seleccion = input('Ahora escoja una opcion: ')
+        if(int(seleccion) == 5):
+            no()
+            break
+        else:            
+            resultado = operaciones[seleccion](int(numero), int(numero_2))
+            print("El resultado es: " + str(resultado) )       
+            opcion = {'1': 'si', '2': no}
+            MenuOpcion()
+            select = input("Escoja una opcion ")
+            result = opcion[select]
+            if(int(select) == 1):
+                True
+            else:
+                no()
+                break
 except ValueError:
     print("Escoga una opcion valida")
